097432915795dc28a91657fa4978b150151f4e7c
germancrc <german.cruz36@gmail.com>
2022-07-08 19:54:44 -0400

    img added

--- js/main.js
+++ js/main.js
@@ -17,3 +17,42 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
+//fetch photos
+
+const getData = async () => {
+  await fetch('https://jsonplaceholder.typicode.com/photos?_delay=5000')
+    .then((response) => {
+      return response.json();
+    })
+    .then((photos) => {
+      console.log(photos);
+
+      const spinner = document.getElementById("spin");
+      //spinner.className = "d-none"
+
+      for (let i = 0; i < 12; i++) {
+
+        const cardCont = document.createElement("div");
+        document.getElementById("juegos-container").appendChild(cardCont);
+
+        const cardBody= document.createElement("div");
+        cardBody.className = "card-body";
+        cardCont.appendChild(cardBody);
+
+        const pics = document.createElement("img");
+        pics.src = photos[i].thumbnailUrl;
+        pics.className = "card-img-top";
+        cardBody.appendChild(pics);
+
+        const title = document.createElement("h6");
+        title.innerText = photos[i].title;
+        title.className = "card-title";
+        cardBody.appendChild(title);
+      }
+    });
+};
+
+getData();
